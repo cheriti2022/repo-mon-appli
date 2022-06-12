b3e4aaf1a9ac3a3832f3e0fc3956952a7a208aac
Your Name <you@example.com>
2022-06-12 23:00:25 +0100

    hello.java

---
+++ src/main/java/com/formation/integcontinue/hello.java
@@ -0,0 +1,13 @@
+package com.formation.integcontinue;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour" );
+    }
+}
