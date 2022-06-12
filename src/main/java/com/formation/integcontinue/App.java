8526ee500f24c839db29f3e92685c37ed4d92ded
cheriti2022 <107357833+cheriti2022@users.noreply.github.com>
2022-06-12 22:49:06 +0100

    Update App.java

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World to github" );
+        System.out.println( "Hello World for github" );
     }
 }
